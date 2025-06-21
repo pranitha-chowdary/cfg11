da3ef28d89af09e6ce16d94eeb75eb8fc7fc56ce
pranitha-chowdary <lagadapatipranitha@gmail.com>
2025-06-21 18:51:23 +0530

    Delete src directory

--- src/seller/ManageOrders.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function ManageOrders() {
-  return (
-    <div>ManageOrders</div>
-  )
-}
--- src/seller/SellerNavBar.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function SellerNavBar() {
-  return (
-    <div>SellerNavBar</div>
-  )
-}
