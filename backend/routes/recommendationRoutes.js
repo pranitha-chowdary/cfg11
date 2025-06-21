2fb8c0c50776854ca3d46d6edaba999a2ef4b4fe
balanagasri <balanagasrithota2005@gmail.com>
2025-06-22 01:21:04 +0530

    Added frontend files to frontend-ui branch

--- backend/routes/recommendationRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const { getRecommendations } = require('../controllers/recommendationController');
-
-const router = express.Router();
-
-router.get('/:buyerId', getRecommendations);  // GET /api/recommendations/:buyerId
-
-module.exports = router;
